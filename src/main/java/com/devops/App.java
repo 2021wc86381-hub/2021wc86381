2cf195da108c42e56640d59d99cb1dbde81522e1
2021wc86381-hub <2021wc86381@wilp.bits-pilani.ac.in>
2026-09-23 21:11:23 +0530

    Initial Maven Project

---
+++ src/main/java/com/devops/App.java
@@ -0,0 +1,9 @@
+package com.devops;
+
+public class App {
+public static void main(String[] args) {
+System.out.println("Hello from Devops Lab!");
+System.out.println("BITS ID: 2021wc86381");
+System.out.println("maven build project completed successfully");
+}
+}
